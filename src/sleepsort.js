093702da10089cce97a217c36952fa30ef93dc2d
Anton Rissanen <hello@anton.fi>
2012-09-08 17:43:57 +0300

    Optimize and clean up code

--- src/sleepsort.js
+++ src/sleepsort.js
@@ -3,19 +3,14 @@
 // Sorts an array of numbers using setTimeout
 
 exports.sleepSort = function sleepSort(array, callback) {
-	var length = array.length;
-	var done = 0;
+	var i = array.length;
 	var sortedArray = [];
-	var add = function(num) {
-		sortedArray.push(num);
-		if (++done === length) {
-			callback(sortedArray);
-		}
-	};
+
 	array.forEach(function(num) {
-		if (typeof num !== 'number') {
-			throw new Error('Sleep sort works only with arrays of numbers');
-		}
-		setTimeout(function() { add(num); }, num);
+		if (typeof num !== 'number') throw new Error('Sleep sort works only with arrays of numbers');
+		setTimeout(function() {
+			sortedArray.push(num);
+			if (!--i) callback(sortedArray);
+		}, num);
 	});
 };
