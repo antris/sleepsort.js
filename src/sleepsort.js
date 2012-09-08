e146eef99889ba20a999a4b914cfb300ca57f0b0
Anton Rissanen <hello@anton.fi>
2012-09-08 18:03:18 +0300

    Return arrays that don't need to be sorted immediately

--- src/sleepsort.js
+++ src/sleepsort.js
@@ -4,6 +4,12 @@
 
 exports.sleepSort = function sleepSort(array, callback) {
 	var i = array.length;
+
+	if (i <= 1) { // No need to sort arrays shorter than 2
+		callback(array);
+		return;
+	}
+
 	var sortedArray = [];
 
 	array.forEach(function(num) {
